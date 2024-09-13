4418f84b6abd0165b9d7826715ab5c3158f9609b
Mobin Valikhani <mobinvalikhani@gmail.com>
2024-09-14 01:59:25 +0330

    fix: add default status code

--- Resulver.AspNetCore.WebApi/ErrorHandling/ErrorResponse.cs
+++ Resulver.AspNetCore.WebApi/ErrorHandling/ErrorResponse.cs
@@ -4,7 +4,7 @@ namespace Resulver.AspNetCore.WebApi.ErrorHandling;
 
 public class ErrorResponse
 {
-    public int StatusCode { get; private set; }
+    public int StatusCode { get; private set; } = StatusCodes.Status400BadRequest;
     public Func<ResultError, IActionResult> Handler { get; private set; }
     public Type ErrorType { get; }
 
@@ -12,7 +12,7 @@ public class ErrorResponse
     {
         Handler = error => new ObjectResult(error)
         {
-            StatusCode = StatusCodes.Status400BadRequest
+            StatusCode = StatusCode
         };
         ErrorType = errorType;
     }
