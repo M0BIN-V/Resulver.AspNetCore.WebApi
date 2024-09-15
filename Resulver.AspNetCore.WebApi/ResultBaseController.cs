0901a291e323e50b72312d0a26638ebed21aaea1
Mobin Valikhani <mobinvalikhani@gmail.com>
2024-09-15 16:57:25 +0330

    feat: add overload FromResult method

--- Resulver.AspNetCore.WebApi/ResultBaseController.cs
+++ Resulver.AspNetCore.WebApi/ResultBaseController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
+using Resulver.AspNetCore.WebApi.Response;
 
 namespace Resulver.AspNetCore.WebApi;
 
@@ -12,13 +13,35 @@ public class ResultBaseController : ControllerBase
 
     protected IActionResult FromResult(Result result, Func<IActionResult> successAction)
     {
-        if (result.IsSuccess)
-        {
-            return successAction();
-        }
-        else
+        if (result.IsFailure)
         {
             return ErrorResponseGenerator.MakeResponse(result.Errors[0]);
         }
+
+        return successAction();
+    }
+    protected IActionResult FromResult(Result result, int successStatusCode = 200)
+    {
+        if (result.IsFailure)
+        {
+            return ErrorResponseGenerator.MakeResponse(result.Errors[0]);
+        }
+
+        return new ObjectResult(result.ToResponseTemplate())
+        {
+            StatusCode = successStatusCode
+        };
+    }
+    protected IActionResult FromResult<TContent>(Result<TContent> result, int successStatusCode = 200)
+    {
+        if (result.IsFailure)
+        {
+            return ErrorResponseGenerator.MakeResponse(result.Errors[0]);
+        }
+
+        return new ObjectResult(result.ToResponseTemplate())
+        {
+            StatusCode = successStatusCode
+        };
     }
 }
